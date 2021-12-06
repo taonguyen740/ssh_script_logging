630f5e3a12812b6c8b2a63f4b1251caa56b0bdf6
taonguyen740 <dinhtao995@gmail.com>
2021-12-06 10:56:42 +0900

    Create make_scriptlog.sh

---
+++ make_scriptlog.sh
@@ -0,0 +1,18 @@
+cat <<EOF >/etc/profile.d/scriptlog.sh
+#===================================
+# Script Name   : scriptlog.sh
+# Description   : This script outputs the operation log for each user to the specified directory.
+#===================================
+
+LOGDIR=/var/log/script
+LOGFILE=\`whoami\`_\`date '+%Y%m%d%H%M%S'\`.log
+
+P_PROC=\`ps -ef|grep \$PPID|grep bash|awk '{print \$8}'\`
+
+if [ "\`whoami\`" != "root" ] && [ "\$P_PROC" = -bash ];then
+  #/bin/script -faq \${LOGFILE}
+  # タイムスタンプを追加
+  /bin/script -faq >(awk '{print strftime("%Y-%m-%d %H:%M:%S "), \$0} {fflush() }'>> \${LOGDIR}/\${LOGFILE})
+  exit
+fi
+EOF
